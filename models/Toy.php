f52fcee6935735b94c813ec8a13b7bcf3e038456
CuonoMaione <cuonomaione99@outlook.it>
2023-07-21 09:25:17 +0200

    #

---
+++ models/Toy.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once __DIR__ . '/Product.php';
+
+
+
+class Toy extends Product {
+    
+    public $size;
+    public $material;
+
+    function __construct(String $name,String $description,Float $price, String $image, Bool $inStock, Category $category, Int $quantity, Int $shelfNo,String $laneNo, String $size, String $material, ){
+        parent::__construct($name, $description, $price, $image, $inStock, $category, $quantity, );
+        $this->size = $size;
+        $this->material = $material;
+        
+    }
+}
